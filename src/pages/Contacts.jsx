60cf9148a9a44f498e7d80601e11f30466107f34
unknown <jack22toplolkek@gmail.com>
2023-09-25 21:47:16 +0300

    fix styles

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,11 +1,12 @@
 import Form from '../components/Form/Form';
 import { ContactsList } from '../components/ContactsList/ContactsList';
 import { Filter } from '../components/Filter/Filter';
+import css from './Contacts.module.css';
 
 export default function Contacts() {
   return (
     <main>
-      <section>
+      <section className={css.section_wrapper}>
         <Form />
         <Filter />
         <ContactsList />
